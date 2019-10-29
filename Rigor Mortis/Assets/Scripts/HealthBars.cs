4b0a28eae2d0cf60c8e8cae1657f420af46e207d
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2019-10-29 19:43:24 +0000

    Started HealthBars

---
+++ Rigor Mortis/Assets/Scripts/HealthBars.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBars : MonoBehaviour
+{
+    Slider[] healthBars;
+    GameObject[] units;
+    // Start is called before the first frame update
+    void Start()
+    {
+        units = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    void InstantiateHealthBar()
+    {
+        foreach(GameObject unit in units)
+        {
+
+        }
+    }
+}
