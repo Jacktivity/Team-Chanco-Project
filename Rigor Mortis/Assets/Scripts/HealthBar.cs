21743013bbd8a6c91b389e671f687a17b123f40e
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2019-10-31 19:48:47 +0000

    First version of healthbar done

---
+++ Rigor Mortis/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject unit;
+    public float offset = 0.75f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(unit.transform.position.x, unit.transform.position.y + offset,  unit.transform.position.z);
+    }
+}
