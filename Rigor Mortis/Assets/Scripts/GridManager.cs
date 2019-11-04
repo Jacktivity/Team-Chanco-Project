07f5740a363aa6862453312c3295dabb24bd6fb7
J.H.Hurst U1669330 <U1669330@unimail.hud.ac.uk>
2019-11-04 12:17:37 +0000

    Can place enemies from xml, map doesn't explode anymore

--- Rigor Mortis/Assets/Scripts/GridManager.cs
+++ Rigor Mortis/Assets/Scripts/GridManager.cs
@@ -9,12 +9,21 @@ using System.Linq;
 
 public class GridManager : MonoBehaviour
 {
-    public GameObject[] tiles;
+    [SerializeField] private GameObject[] tiles;
+    [SerializeField] private GameObject enemyContainter;
+    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private GameObject playerContainter;
+    [SerializeField] private GameObject[] playerPrefabs;
+    [SerializeField] private TextAsset levelMap;
+
+    xml.levels xmlData;
 
     // Start is called before the first frame update
     void Start()
     {
+        xmlData = XmlReader<xml.levels>.ReadXMLAsBytes(levelMap.bytes);
         GenerateLevel();
+        PlaceEnemy();
     }
 
     /**
@@ -26,21 +35,37 @@ public class GridManager : MonoBehaviour
      * */
     void GenerateLevel()
     {
-        var xml = XmlReader<xml.levels>.ReadXML("./Assets/Levels/test_map.xml");
+        var level = xmlData.level;
+       
+        var maplines = level.map.Select(m => m.value.Split(',').Select(v => int.Parse(v)).ToArray()).ToArray();
 
-        var map = xml.level.First(m => m.name == "test-level");
-        var maplines = map.mapline.Select(m => m.value.Split(',').Select(v => int.Parse(v)).ToArray()).ToArray();
-
-        var anonMap = map.mapline.SelectMany((m,x) => m.value.Split(',').Select((v,z) => new { Value = int.Parse(v), ZPos = z, XPos = x })).ToArray();
+        var anonMap = level.map.SelectMany((m, x) => m.value.Split(',').Select((v, z) => new { Value = int.Parse(v), ZPos = z, XPos = x })).ToArray();
         foreach(var pos in anonMap)
         {
-            if(pos.Value >= 0)
+            if (pos.Value >= 0)
             {
-                GameObject tile = Instantiate(tiles[pos.Value], new Vector3(pos.XPos, 0, pos.ZPos), new Quaternion());
+                GameObject tile = Instantiate(tiles[pos.Value], new Vector3(pos.XPos, 0, pos.ZPos), new Quaternion(), gameObject.transform);
                 tile.GetComponent<BlockScript>().coordinates = new Vector3(pos.XPos, 0, pos.ZPos);
+                tile.name = tile.name.Replace("(Clone)", "");
+                tile.name = tile.name + '(' + pos.XPos + ',' + pos.ZPos + ')';
             }
         }
     }
+
+    void PlaceEnemy()
+    {
+        var level = xmlData.level;
+        var enemies = level.enemies;
+
+        foreach(var enemy in enemies)
+        {
+            GameObject placedEnemy = Instantiate(enemyPrefabs[enemy.type], new Vector3(enemy.posX, 1, enemy.posZ), new Quaternion(), enemyContainter.transform);
+            placedEnemy.name = enemy.name;
+        }
+        GameObject placedPlayer = Instantiate(playerPrefabs[0], new Vector3( 7,1, 10), new Quaternion(), playerContainter.transform);
+        placedPlayer.name = "target";
+    }
+
     /**
      * This is the xml reader that will get the file by path
      * Serialise the data of the xml
@@ -63,10 +88,24 @@ public class GridManager : MonoBehaviour
                 }
             }
         }
+
+        public static T ReadXMLAsBytes(byte[] xmlData)
+        {
+            var _serializer = new XmlSerializer(typeof(T));
+
+            using (var memoryStream = new MemoryStream(xmlData))
+            {
+                using (var reader = new XmlTextReader(memoryStream))
+                {
+                    return (T)_serializer.Deserialize(reader);
+                }
+            }
+        }
     }
 }
 namespace xml
 {
+
 
     // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
     /// <remarks/>
@@ -77,11 +116,10 @@ namespace xml
     public partial class levels
     {
 
-        private levelsLevel[] levelField;
+        private levelsLevel levelField;
 
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute("level")]
-        public levelsLevel[] level
+        public levelsLevel level
         {
             get
             {
@@ -101,35 +139,35 @@ namespace xml
     public partial class levelsLevel
     {
 
-        private levelsLevelMapline[] maplineField;
+        private levelsLevelMapline[] mapField;
 
-        private string nameField;
+        private levelsLevelEnemy[] enemiesField;
 
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute("map-line")]
-        public levelsLevelMapline[] mapline
+        [System.Xml.Serialization.XmlArrayItemAttribute("map-line", IsNullable = false)]
+        public levelsLevelMapline[] map
         {
             get
             {
-                return this.maplineField;
+                return this.mapField;
             }
             set
             {
-                this.maplineField = value;
+                this.mapField = value;
             }
         }
 
         /// <remarks/>
-        [System.Xml.Serialization.XmlAttributeAttribute()]
-        public string name
+        [System.Xml.Serialization.XmlArrayItemAttribute("enemy", IsNullable = false)]
+        public levelsLevelEnemy[] enemies
         {
             get
             {
-                return this.nameField;
+                return this.enemiesField;
             }
             set
             {
-                this.nameField = value;
+                this.enemiesField = value;
             }
         }
     }
@@ -154,6 +192,78 @@ namespace xml
             set
             {
                 this.valueField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class levelsLevelEnemy
+    {
+
+        private string nameField;
+
+        private byte typeField;
+
+        private byte posXField;
+
+        private byte posZField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte posX
+        {
+            get
+            {
+                return this.posXField;
+            }
+            set
+            {
+                this.posXField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte posZ
+        {
+            get
+            {
+                return this.posZField;
+            }
+            set
+            {
+                this.posZField = value;
             }
         }
     }
