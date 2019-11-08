08255f977488c3da84fb2b363b2f4b0e768e7b33
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2019-11-08 12:21:46 +0000

    Camera added

--- Rigor Mortis/Assets/Scripts/UI/Camera.cs
+++ Rigor Mortis/Assets/Scripts/UI/Camera.cs
@@ -8,36 +8,77 @@ public class Camera : MonoBehaviour
     [SerializeField]BoxCollider _collider;
 
     [SerializeField]private float speed = 25f;
-    [SerializeField]private float scrollSpeed = 300f;
+    [SerializeField]private float scrollSpeed = 50f;
 
-    Vector3 direction;
+    [SerializeField] private float scrollOffset = 4;
+
+    Vector3 posColliderExtents;
+    Vector3 negColliderExtents;
 
     // Start is called before the first frame update
     void Start()
     {
         _camera = GetComponent<Camera>();
+        posColliderExtents = _collider.transform.position + _collider.bounds.extents;
+        negColliderExtents = _collider.transform.position - _collider.bounds.extents;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        direction = _camera.transform.TransformDirection( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) );
+        //Camera Movement
+        Vector3 direction = _camera.transform.TransformDirection( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) );
+        Vector3 scroll = _camera.transform.TransformDirection(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
 
-        Vector3 posColliderExtents = _collider.transform.position + _collider.bounds.extents;
-        Vector3 negColliderExtents = _collider.transform.position - _collider.bounds.extents;
-
-        if(transform.position.x >= posColliderExtents.x || transform.position.x <= negColliderExtents.x) {
-            transform.position += speed * new Vector3( direction.x, 0, direction.z ) * Time.deltaTime;
-        } else {
-            Debug.Log(transform.position.x  + " " + posColliderExtents.x + negColliderExtents.x );
+        if ((transform.position.x + direction.x) >= posColliderExtents.x)
+        {
+            direction.x = 0;
+        }
+        if((transform.position.x + direction.x) <= negColliderExtents.x)
+        {
+            direction.x = 0;
+        }
+        if((transform.position.z + direction.z) >= posColliderExtents.z)
+        {
+            direction.z = 0;
+        }
+        if((transform.position.z + direction.z) <= negColliderExtents.z)
+        {
+            direction.z = 0;
         }
 
-        /*if (Input.GetAxisRaw( "Horizontal" ) != 0 || Input.GetAxisRaw( "Vertical" ) != 0) {
-            transform.position += speed * new Vector3( direction.x, 0, direction.z ) * Time.deltaTime;
-        }*/
+        transform.position += speed * new Vector3( direction.x, 0, direction.z ) * Time.deltaTime;
 
-        if (Input.GetAxis( "Mouse ScrollWheel" ) != 0) {
-            transform.position += scrollSpeed * new Vector3( 0, -Input.GetAxis( "Mouse ScrollWheel" ), 0 ) * Time.deltaTime;
+        //Zoom Movement
+        if (scroll.y != 0) {
+            scroll.x = -(scroll.x / scrollOffset);
+            scroll.z = -(scroll.z / scrollOffset);
+
+            if((transform.position.x + scroll.x) >= posColliderExtents.x)
+            {
+                scroll = new Vector3();
+            }
+            if ((transform.position.x + scroll.x) <= negColliderExtents.x)
+            {
+                scroll = new Vector3();
+            }
+            if ((transform.position.y + scroll.y) >= posColliderExtents.y)
+            {
+                scroll = new Vector3();
+            }
+            if((transform.position.y + scroll.y) <= negColliderExtents.y)
+            {
+                scroll = new Vector3();
+            }
+            if ((transform.position.z + scroll.z) >= posColliderExtents.z)
+            {
+                scroll = new Vector3();
+            }
+            if ((transform.position.z + scroll.z) <= negColliderExtents.z)
+            {
+                scroll = new Vector3();
+            }
+            transform.position += scrollSpeed * new Vector3(scroll.x, scroll.y, scroll.z) * Time.deltaTime;
         }
     }
 }
