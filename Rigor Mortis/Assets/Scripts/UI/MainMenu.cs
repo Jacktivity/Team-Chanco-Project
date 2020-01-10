0a6f52403fb4185048023da47a02bf9d530126f1
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2020-01-10 17:30:13 +0000

    Main Menu started

---
+++ Rigor Mortis/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas mainCanvas;
+    public Canvas levelCanvas;
+
+    private void Start()
+    {
+        levelCanvas.enabled = false;
+    }
+
+    public void CanvasSwitch()
+    {
+        levelCanvas.enabled = !levelCanvas.enabled;
+        mainCanvas.enabled = !mainCanvas.enabled;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
