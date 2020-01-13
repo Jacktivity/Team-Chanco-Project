8dc62d6f214661b4bf95113a4fa52a95d2633f32
OliverSheard <55920246+OliverSheard@users.noreply.github.com>
2020-01-13 17:06:25 +0000

    Moving and Attacking updated
    Using AP system correctly
    Bug - End turn not being called

--- Rigor Mortis/Assets/Scripts/UI/MoveButton.cs
+++ Rigor Mortis/Assets/Scripts/UI/MoveButton.cs
@@ -10,27 +10,35 @@ public class MoveButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     private Character character;
     private BlockScript[] walkTiles, sprintTiles;
     private GridManager gridManager;
+    private UIManager uiManager;
     private bool hideGridMoveOnExit = true;
 
     // Start is called before the first frame update
     void Start()
     {
         gridManager = FindObjectOfType<GridManager>();
+        uiManager = FindObjectOfType<UIManager>();
     }
 
     public void SetUpMoveButton(Character unit)
     {
         character = unit;
-        if(unit.CanAttack)
+        if(unit.MaxAP)
         {
-            sprintTiles = unit.pathfinder.GetTilesInRange(unit.floor, unit.movemenSprint, unit.isFlying, false);
+            sprintTiles = unit.pathfinder.GetTilesInRange(unit.floor, unit.movemenSprint + unit.movementSpeed, unit.isFlying, false);
+            walkTiles = unit.pathfinder.GetTilesInRange(unit.floor, unit.movementSpeed, unit.isFlying, false);
+        }
+        else if(unit.HasAttacked)
+        {
+            sprintTiles = new BlockScript[0];
             walkTiles = unit.pathfinder.GetTilesInRange(unit.floor, unit.movementSpeed, unit.isFlying, false);
         }
         else
         {
             sprintTiles = new BlockScript[0];
-            walkTiles = unit.pathfinder.GetTilesInRange(unit.floor, unit.movemenSprint - unit.movementSpeed, unit.isFlying, false);
+            walkTiles = unit.pathfinder.GetTilesInRange(unit.floor, unit.movemenSprint, unit.isFlying, false);
         }
+
     }
 
     public void ButtonClicked()
@@ -43,7 +51,13 @@ public class MoveButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     private void MoveCharacterCheck(object sender, BlockScript e)
     {
         if (sprintTiles.Contains(e) || walkTiles.Contains(e))
-            character.MoveUnit(character.pathfinder.GetPath(character.floor, (block) => block == e, character.isFlying == false));
+        {
+            BlockScript.blockClicked -= MoveCharacterCheck;
+            character.MoveUnit(character.pathfinder.GetPath(character.floor, (block) => block == e, character.isFlying == false));            
+            gridManager.ClearMap();
+            gridManager.ClearMap();
+            uiManager.DeleteCurrentPopupButtons();
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
