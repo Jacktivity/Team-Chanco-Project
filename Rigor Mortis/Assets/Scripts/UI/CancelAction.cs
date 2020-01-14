53da3801c835c0bbf0f04b0577f461a657d6ca1f
OliverSheard <55920246+OliverSheard@users.noreply.github.com>
2020-01-14 10:23:29 +0000

    Morning update

---
+++ Rigor Mortis/Assets/Scripts/UI/CancelAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelAction : MonoBehaviour
+{
+    private UIManager uiManager;
+    public void Start()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+
+
+    public void CancelCurrentAction()
+    {
+        uiManager.DeleteCurrentPopupButtons();
+    }
+}
