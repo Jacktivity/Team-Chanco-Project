4a419cacd8d18e5fa697863ec3879044f4b6caaa
OliverSheard <55920246+OliverSheard@users.noreply.github.com>
2020-01-14 10:46:21 +0000

    Added CancelAction button
    Needs implementing into scene,
    Resets UI to the actions a character can perform

--- Rigor Mortis/Assets/Scripts/UI/CancelAction.cs
+++ Rigor Mortis/Assets/Scripts/UI/CancelAction.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class CancelAction : MonoBehaviour
 {
     private UIManager uiManager;
+    private Character character;
     public void Start()
     {
         uiManager = FindObjectOfType<UIManager>();
     }
 
-
+    public void SetCharacter(Character character)
+    {
+        this.character = character;
+    }
 
     public void CancelCurrentAction()
     {
         uiManager.DeleteCurrentPopupButtons();
+        uiManager.DisplayActionButtons(character.attacks, character);
     }
 }
