782c43993799a3a065292ea016c039b375445f52
J.H.Hurst U1669330 <U1669330@unimail.hud.ac.uk>
2020-01-15 17:42:31 +0000

    Skull stats

---
+++ Rigor Mortis/Assets/Scripts/Character/FloatingSkullCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FloatingSkullCharacter : Character
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = "FlamingSkull";
+        cost = 15;
+        maxHitPoints = 12;
+        accuracy = 90;
+        armour = 1;
+        evade = 25;
+        manaPoints = 50;
+        power = 1;
+        resistance = 1;
+        movementSpeed = 5;
+        movemenSprint = 7;
+
+        currentHitPoints = maxHitPoints;
+
+        attacks = new Attack[] { AttackLibrary.RustySword };
+    }
+}
