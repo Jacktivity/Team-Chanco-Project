748ccd937b448b656806c317416cbdff1c595dd4
Jacktivity <140695@hurst>
2020-01-17 14:19:30 +0000

    Added new units and fixed levels

--- Rigor Mortis/Assets/Scripts/Character/SkeletonCharacterAxe.cs
+++ Rigor Mortis/Assets/Scripts/Character/SkeletonCharacterAxe.cs
@@ -22,6 +22,6 @@ public class SkeletonCharacterAxe : Character
         
         currentHitPoints = maxHitPoints;
 
-        attacks = new Attack[] { AttackLibrary.RustySword };
+        attacks = new Attack[] { AttackLibrary.Axe };
     }
 }
