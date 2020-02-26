0a9386cb18d58ab84cf5931768de2e801b314aed
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2020-02-26 11:59:34 +0000

    Update PersistantData.cs

--- Rigor Mortis/Assets/Scripts/UI/PersistantData.cs
+++ Rigor Mortis/Assets/Scripts/UI/PersistantData.cs
@@ -16,7 +16,6 @@ public static class PersistantData
     public static void AddScore(int amount)
     {
         score += amount;
-        Debug.Log("SCORE NOW EQUALS: " + score);
     }
 
     public static float GetScore()
@@ -27,23 +26,19 @@ public static class PersistantData
     public static float EndTurnWin(int turns, int par)
     {
         float spdMP = 1;
-        Debug.Log("Initial Speed Multiplier : " + spdMP + " Par: " + par + " Turns: " + turns);
         if(turns < par) {
             for(int i = 0; i < (par - turns); i++)
             {
                 spdMP = spdMP + 0.1f;
-                Debug.Log("Speed Multiplier : " + spdMP);
             }
         } else if(turns > par) {
             for (int i = 0; i < (turns - par); i++)
             {
                 if (spdMP > 0.1) { //What to do if more than 10 turns under par?
                     spdMP = spdMP - 0.1f;
-                    Debug.Log("Speed Multiplier : " + spdMP);
                 }
             }
         }
-        Debug.Log("Score : " + (score * spdMP));
         score = Mathf.RoundToInt(score * spdMP);
         return score;
     }
