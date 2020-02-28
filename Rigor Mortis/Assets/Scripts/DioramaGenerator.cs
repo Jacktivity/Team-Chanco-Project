4c7bf568aefbc21ff83c0b80bb292d16fad8d5b8
OliverSheard <55920246+OliverSheard@users.noreply.github.com>
2020-02-28 14:30:38 +0000

    Pre merge commit
    Adding to edge of map dioramas

--- Rigor Mortis/Assets/Scripts/DioramaGenerator.cs
+++ Rigor Mortis/Assets/Scripts/DioramaGenerator.cs
@@ -6,6 +6,7 @@ using System.Linq;
 
 public class DioramaGenerator : MonoBehaviour
 {
+    public Vector3 min, max;
     [SerializeField] private GameObject[] dioramaPieces;
     [SerializeField] private GameObject dioramaParent;
     // Start is called before the first frame update
@@ -16,29 +17,44 @@ public class DioramaGenerator : MonoBehaviour
 
     private void GenerateDiorama(object sender, BlockScript[] e)
     {
-        var min = e.First(s => s.coordinates == new Vector3(0, 0, 0)).transform.position;
-        var max = new Vector3(e.OrderByDescending(t => t.coordinates.x).First().transform.position.x,0, e.OrderByDescending(t => t.coordinates.z).First().transform.position.z);
+        min = e.First(s => s.coordinates == new Vector3(0, 0, 0)).transform.position;
+        max = new Vector3(e.OrderByDescending(t => t.coordinates.x).First().transform.position.x,0, e.OrderByDescending(t => t.coordinates.z).First().transform.position.z);
 
-        FillEdge(min, max, min - new Vector3(max.x, 0, 0));
-        FillEdge(min, max, min + new Vector3(max.x, 0, 0));
-        FillEdge(min, max, min + new Vector3(0, 0, max.z));
-        FillEdge(min, max, min - new Vector3(0, 0, max.z));
-        FillEdge(min, max, min + new Vector3(-max.x, 0, max.z));
-        FillEdge(min, max, min - new Vector3(max.x, 0, max.z));
-        FillEdge(min, max, min + new Vector3(max.x, 0, -max.z));
-        FillEdge(min, max, max);
+        var topLeft = min - new Vector3(max.x, 0, max.z);
+        var topMid = min - new Vector3(max.x, 0, 0);
+        var topRight = min + new Vector3(-max.x, 0, max.z);
+        var midRight = min + new Vector3(0, 0, max.z);
+        var midLeft = min - new Vector3(0, 0, max.z);
+        var bottomLeft = min + new Vector3(max.x, 0, -max.z);
+        var bottomMid = min + new Vector3(max.x, 0, 0);
+
+        FillEdge(topLeft, topMid, "TopLeft");
+        FillEdge(topMid, topRight, "TopMid");
+        FillEdge(topRight, topRight * 2, "TopRight");        
+        FillEdge(midRight, midRight, "MidRight");
+        FillEdge(midLeft, midLeft, "MidLeft");     
+        FillEdge(bottomLeft, bottomLeft, "BtmLeft");
+        FillEdge(bottomMid, bottomMid, "BtmMid");
+        FillEdge(max, max, "BtmRight");
     }
 
-    private void FillEdge(Vector3 min, Vector3 max, Vector3 origin)
+    private void FillEdge(Vector3 origin, Vector3 endPoint, string name)
     {
-        var piece = Instantiate(RandPiece, origin, new Quaternion(), dioramaParent.transform);
-        var bounds = piece.GetComponent<Collider>().bounds.max;
-        var scale = piece.transform.localScale;
+        var spawnedPiece = RandPiece;
+        var spawnAt = origin + max - min;
+        var endAt = origin + max - min;
+        
+        var bounds = spawnedPiece.GetComponent<Collider>().bounds.max;
+        var scale = spawnedPiece.transform.localScale;
 
-        var scaleRequirements = new float[] { scale.x / bounds.x, scale.z / bounds.z };
-        var scaleFactor = scaleRequirements.Max();
+        var scalar = endPoint - origin;
+        var scaleFactor = scalar.x > scalar.z? bounds.x / (endPoint.x - origin.x) : bounds.z / (endPoint.z - origin.z);
 
-        piece.transform.localScale = new Vector3(scale.x * scaleFactor, scale.y * scaleFactor, scale.z * scaleFactor);
+
+        spawnAt += spawnedPiece.GetComponent<Collider>().bounds.center;
+        var piece = Instantiate(spawnedPiece, spawnAt, new Quaternion(), dioramaParent.transform);
+        piece.name = name + scaleFactor;
+        //piece.transform.localScale = new Vector3(scale.x * scaleFactor, scale.y * scaleFactor, scale.z * scaleFactor);
     }
 
     private GameObject RandPiece => dioramaPieces[UnityEngine.Random.Range(0, dioramaPieces.Length - 1)];
