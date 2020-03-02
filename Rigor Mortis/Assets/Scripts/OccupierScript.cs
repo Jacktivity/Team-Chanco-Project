6b915e9c5eecff49a4540aff6f174ffb415fc498
J.H.Hurst U1669330 <U1669330@unimail.hud.ac.uk>
2020-03-02 13:21:01 +0000

    crypt fix and script

--- Rigor Mortis/Assets/Scripts/OccupierScript.cs
+++ Rigor Mortis/Assets/Scripts/OccupierScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crypt : MonoBehaviour
+public class OccupierScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
